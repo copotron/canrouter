76b890a2d67f1272d1f10a6ffc46b6e1cdeff2d2
Maruf Maniruzzaman <kuasha@users.noreply.github.com>
2018-02-17 17:28:32 -0800

    Create canreader.ino

---
+++ src/canreader.ino
@@ -0,0 +1,57 @@
+// Demo code from Arduino CAN BUS Shield library
+// With modifications by Maruf Maniruzzaman
+
+#include <SPI.h>
+#include "mcp_can.h"
+
+// the cs pin of the version after v1.1 is default to D9
+// v0.9b and v1.0 is default D10
+const int SPI_CS_PIN = 9;
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+
+unsigned char flagRecv = 0;
+unsigned char len = 0;
+unsigned char buf[8];
+char str[20];
+
+void setup()
+{
+    Serial.begin(115200);
+
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS Shield init fail");
+        Serial.println(" Init CAN BUS Shield again");
+        delay(100);
+    }
+    
+    Serial.println("CAN BUS Shield init ok!");
+    attachInterrupt(0, MCP2515_ISR, FALLING); // start interrupt
+}
+
+void MCP2515_ISR()
+{
+    flagRecv = 1;
+}
+
+void loop()
+{
+    if(flagRecv) 
+    {                                   // check if get data
+        flagRecv = 0;                   // clear flag
+        while (CAN_MSGAVAIL == CAN.checkReceive()) 
+        {
+            // read data,  len: data length, buf: data buf
+            CAN.readMsgBuf(&len, buf);
+
+            // print the data
+            for(int i = 0; i<len; i++)
+            {
+                Serial.print(buf[i]);Serial.print(" ");
+            }
+            Serial.println();
+        }
+    }
+}
